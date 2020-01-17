87cff5f2eeabf5bec1856274e22a92143cddd32c
Chris York <christopherayork1992@gmail.com>
2020-01-17 11:53:50 -0500

    hash 1 done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -7,13 +7,24 @@ from hashtables import (HashTable,
 
 
 def get_indices_of_item_weights(weights, length, limit):
-    ht = HashTable(16)
+    ht = HashTable(length * 2)
 
     """
     YOUR CODE HERE
     """
-
-    return None
+    for x in range(len(weights)):
+        hash_table_insert(ht, weights[x], x)
+    answer = None
+    for x in range(len(weights)):
+        target = limit - weights[x]
+        current = weights[x]
+        current_index = x
+        valid = hash_table_retrieve(ht, target)
+        if valid is not None:
+            if x > valid: answer = (x, valid)
+            else: answer = (valid, x)
+            return answer
+    return answer
 
 
 def print_answer(answer):
